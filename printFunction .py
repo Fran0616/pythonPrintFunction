a5c280150e0100fc9621761b956b266ef93406c2
Fran0616 <59398607+Fran0616@users.noreply.github.com>
2020-08-18 21:48:55 -0400

    Add files via upload

---
+++ printFunction .py
@@ -0,0 +1,5 @@
+
+#The sep parameter is primarily used to format the strings that need to be printed on the console and add a separator between strings to be printed
+print("Programming","Essentials","in", sep="***", end="...") 
+#The end parameter is used to append any string at the end of the output of the print statement in python.
+print("Python")
